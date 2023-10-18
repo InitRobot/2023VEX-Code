0430343499f6e94c388144269744b7e0d4e94a9b
victorchenjiantian <vict2008or0411@163.com>
2023-10-18 20:10:45 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       21348                                                     */
-/*    Created:      2023/9/30 15:19:58                                        */
+/*    Author:       Victor                                                    */
+/*    Created:      2023/10/17 18:34:36                                       */
 /*    Description:  V5 project                                                */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
@@ -11,51 +11,80 @@
 
 using namespace vex;
 
-  using signature = vision::signature;
-  using code = vision::code;
-
-  // A global instance of brain used for printing to the V5 Brain screen
-  brain  Brain;
-
-  // VEXcode device constructors
-  
-  //controller
-  controller Controller1 = controller(primary);                             //
 
 
-  
+
+
+
+// A global instance of competition
+competition Competition;
+
+
+
+
+
+// define your global instances of motors and other devices here
+//---------------------------------所有初始化(电机，传感器……)
+
+
+//视觉传感器
+using signature = vision::signature;
+using code = vision::code;
+
   /*vex-vision-config:begin*/
   //-------------------------------------------------------------------------下一行后参数无法理解，抄的code里的（此文件在桌面）-------------------------------------------
   vision::signature Vision_BLUE_BALL = vision::signature (1, -2705, -1873, -2289,7615, 10485, 9050,2.1, 0);
   vision VisionSensor = vision (PORT17, 50, Vision_BLUE_BALL);
   /*vex-vision-config:end*/
-  
-  //distance_sensor
+
+
+//屏幕设置
+  // A global instance of brain used for printing to the V5 Brain screen
+  brain  Brain;
+  // VEXcode device constructors
+
+
+//手柄
+  //controller
+  controller Controller1 = controller(primary);     
+
+
+//distance_sensor
   distance distance_sensor = distance(PORT18);
 
-  //classis
+
+//classis
   motor classis_left = motor(PORT1, ratio18_1, false);                        //bottom_l
   motor classis_right = motor(PORT2, ratio18_1, false);                       //bottom_r
-  motor classis_horizontal = motor(PORT3, ratio18_1, false);                  //Motor3
+  motor classis_horizontal = motor(PORT3, ratio18_1, false);
+  inertial DrivetrainInertial = inertial(PORT21);
+  smartdrive Drivetrain = smartdrive(classis_left, classis_right, DrivetrainInertial, 319.19, 320, 40, mm, 1);
 
-  //launcher
+
+//launcher
   motor launcher_left = motor(PORT4, ratio36_1, false);                    //right_leftMotorA
   motor launcher_right = motor(PORT5, ratio36_1, true);                           //right_leftMotorB
-  motor_group launcher = motor_group(launcher_left, launcher_right);                    //shoot
+  motor_group launcher = motor_group(launcher_left, launcher_right);
 
-  //intake
+
+//intake
   motor intake_left = motor(PORT8, ratio18_1, false);                                 //rotate_left
   motor intake_right = motor(PORT9, ratio18_1, true);                                     //rotate_right
   motor_group intake = motor_group(intake_left, intake_right);                              //rotate
   //motor_group right_left = motor_group(right_leftMotorA, right_leftMotorB);
 
-  // VEXcode generated functions
+
+// VEXcode generated functions
   // define variable for remote controller enable/disable
   bool RemoteControlCodeEnabled = true;
-// A global instance of competition
-competition Competition;
 
-float sensitivity = 0.5;//range:(0,1]...........................................................................require seting<<<<<<<<<<<<<<<<<<<-----
+
+
+float sensitivity = 0.7;//range:(0,1]...........................................................................require seting<<<<<<<<<<<<<<<<<<<-----
+
+
+
+
 
 //The followings are to prevent over voltaging on one side, which leads to a lack of rotation while at high speed.
 void classis_move(float left_volt, float right_volt, float horizontal_volt){
@@ -77,11 +106,10 @@ void classis_move(float left_volt, float right_volt, float horizontal_volt){
   classis_horizontal.spin(forward, horizontal_volt, voltageUnits::mV);
 }
 
-void classis_move_with_speed_in_direction(float speed, float rotation, float direction){
-  //----------------------------------------------------------------------------------------------------required
-}
 
-// define your global instances of motors and other devices here
+
+
+
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -96,7 +124,39 @@ void classis_move_with_speed_in_direction(float speed, float rotation, float dir
 void pre_auton(void) {
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
+
+
+  //底盘初始化
+  DrivetrainInertial.calibrate();
+
+
+
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
@@ -110,20 +170,33 @@ void pre_auton(void) {
 
 void autonomous(void) {
   // ..........................................................................
-  // Insert autonomous user code here.
-  classis_move(12000,12000,0);
-  intake.spin(reverse, 12000, voltageUnits::mV);
-  wait(600, msec);
-  launcher.spin(forward,12000,voltageUnits::mV);
-  
-  wait(1400, msec);
-  //launcher.spin(forward,0,voltageUnits::mV);
-  classis_move(-12000,-12000,0);
-  wait(1100, msec);
-  launcher.spin(forward,0,voltageUnits::mV);
-  classis_move(0,0,0);
+  // Insert autonomous user code here.  
   // ..........................................................................
+    //底盘初始化
+  DrivetrainInertial.calibrate();
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
@@ -137,8 +210,15 @@ void autonomous(void) {
 
 void usercontrol(void) {
   // User control code here, inside the loop
+
+
+  //底盘初始化
+  DrivetrainInertial.calibrate();
   
-  float intake_spin = -12000;
+    // This is the main execution loop for the user control program.
+    // Each time through the loop your program should update motor + servo
+    // values based on feedback from the joysticks.
+    float intake_spin = -12000;
   int ball = 0;//0:stop,1:up,-1:down
   int times = 0;
   int launch = 0;
@@ -185,7 +265,8 @@ void usercontrol(void) {
     {
       ball = -1;
     }
-    if(Controller1.ButtonA.pressing())  {
+    if(Controller1.ButtonA.pressing())  
+    {
       ball = 0;
     }  
     if(Controller1.ButtonR1.pressing())
@@ -211,7 +292,7 @@ void usercontrol(void) {
         launch = 1;
         ball = 1;
     }
-    if (launch == 1 && ball_distance >=200)
+    if (launch == 1 && ball_distance >=250)
     {
       launch = 0;
       ball = 0;
@@ -242,16 +323,78 @@ void usercontrol(void) {
     // This is the main execution loop for the user control program.
     // Each time through the loop your program should update motor + servo
     // values based on feedback from the joysticks.
-
     // ........................................................................
     // Insert user code here. This is where you use the joystick values to
     // update your motors, etc.
     // ........................................................................
 
+
+
+
+
+
+
+
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
   }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //
 // Main will set up the competition functions and callbacks.
